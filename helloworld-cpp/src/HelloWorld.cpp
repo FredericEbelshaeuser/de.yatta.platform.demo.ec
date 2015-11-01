603f2b47b59aabbee243ab7bccf619895876eafa
Frederic Ebelshaeuser <ebelshaeuser@yatta.de>
2015-11-01 21:38:33 +0100

    add cpp project

---
+++ helloworld-cpp/src/HelloWorld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : fred
+// Version     :
+// Copyright   : Yatta Solutions GmbH
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello Eclipse Con" << endl; // prints Hello Eclipse Con
+	return 0;
+}
