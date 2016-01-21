3edb0d9e8943a23eb50c3a4840c9c795471884eb
Frederic Ebelshaeuser <ebelshaeuser@yatta.de>
2016-01-21 11:56:10 +0100

    add maven example project

---
+++ de.yatta.platform.demo.maven/src/main/java/de/yatta/platform/demo/de/yatta/platform/demo/maven/App.java
@@ -0,0 +1,13 @@
+package de.yatta.platform.demo.de.yatta.platform.demo.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
